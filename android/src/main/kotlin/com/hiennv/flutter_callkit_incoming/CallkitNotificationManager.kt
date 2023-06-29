de2ee2659c726f60c72f582f73faed091b1be63b
chanon.pingpong <chanon.pingpong@gmail.com>
2023-06-29 10:56:04 +0700

    remove open app when timeout

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -453,15 +453,8 @@ class CallkitNotificationManager(private val context: Context) {
     }
 
     private fun getTimeOutPendingIntent(id: Int, data: Bundle): PendingIntent {
-//        val timeOutIntent = CallkitIncomingBroadcastReceiver.getIntentTimeout(context, data)
-//        return PendingIntent.getBroadcast(context, id, timeOutIntent, getFlagPendingIntent())
-
-        val intentTransparent = TransparentActivity.getIntent(
-            context,
-            CallkitConstants.ACTION_CALL_TIMEOUT,
-            data
-        )
-        return PendingIntent.getActivity(context, id, intentTransparent, getFlagPendingIntent())
+        val timeOutIntent = CallkitIncomingBroadcastReceiver.getIntentTimeout(context, data)
+        return PendingIntent.getBroadcast(context, id, timeOutIntent, getFlagPendingIntent())
     }
 
     private fun getCallbackPendingIntent(id: Int, data: Bundle): PendingIntent {
